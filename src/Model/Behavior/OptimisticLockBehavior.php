c177eb5f828290bfbcbd33919a007e68c6bacae5
kaihiro <kai.hirotomo@gmail.com>
2016-12-14 18:27:08 +0900

    fix exclusion control bug

--- src/Model/Behavior/OptimisticLockBehavior.php
+++ src/Model/Behavior/OptimisticLockBehavior.php
@@ -17,11 +17,6 @@ use OptimisticLock\Exception\OptimisticLockException;
  */
 class OptimisticLockBehavior extends Behavior
 {
-    protected $_defaultConfig = [
-        // Don't change!! Now, support 'version' only.
-        'version_field' => 'version',
-    ];
-
     /**
      * versionが一致しているかをチェックします.
      *
@@ -37,37 +32,14 @@ class OptimisticLockBehavior extends Behavior
         if (isset($entity->{$this->_table->primaryKey()})) {
             $latestEntity = $this->_table->get($entity->{$this->_table->primaryKey()});
             if (!empty($latestEntity)) {
-                $config = $this->config();
-                $currentVersion = $entity->get($config['version_field']);
-                $latestVersion = $latestEntity->get($config['version_field']);
-                if ($currentVersion != $latestVersion) {
+                $postVersion = $entity->get('version');
+                $this->postVersion = $postVersion;
+                $latestVersion = $latestEntity->get('version');
+                if ($postVersion != $latestVersion) {
                     $event->stopPropagation();
                     throw new OptimisticLockException();
                 }
             }
-        }
-    }
-
-    /**
-     * 更新前にversionをインクリメントして保存します.
-     *
-     * versionの値が未設定の場合は1を設定します。
-     *
-     * @param Event           $event
-     * @param EntityInterface $entity
-     * @param $options
-     */
-    public function beforeSave(Event $event, EntityInterface $entity, $options)
-    {
-        $config = $this->config();
-        if ($entity->has($config['version_field'])) {
-            if ($entity->dirty($config['version_field'])) {
-                return;
-            }
-            $currentVersion = $entity->get($config['version_field']);
-            $entity->set($config['version_field'], $currentVersion + 1);
-        } else {
-            $entity->set($config['version_field'], 1);
         }
     }
 }
